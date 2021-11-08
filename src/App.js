2512b54c06b7201b13a94f77d6a1b1fbfa43ed78
Flor Marshall <flordemarshall@gmail.com>
2021-11-08 09:52:15 -0500

    prepare for test deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { ethers } from "ethers";
+import { ethers } from "ethers";  // eslint-disable-next-line
 import './App.css';
 
 export default function App() {
@@ -12,15 +12,15 @@ export default function App() {
     <div className="mainContainer">
 
       <div className="dataContainer">
-        <div className="header">
+        <div className="header m-2">
         👋 Hey there!
         </div>
 
-        <div className="bio">
-        I am Flor 💮 and I work for FlureeDB, its a graph database with a blockchain, but on my free time I like to learn about web3. Connect your Ethereum wallet and send me a funny meme!
+        <div className="font-mono text-center">
+        I am Flor 💮, I work for FlureeDB, its a graph database with a blockchain, but on my free time I like to learn about web3. Connect your Ethereum wallet and send me a funny meme!
         </div>
 
-        <button className="waveButton" onClick={meme}>
+        <button className="m-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={meme}>
           Send a meme!
         </button>
       </div>
