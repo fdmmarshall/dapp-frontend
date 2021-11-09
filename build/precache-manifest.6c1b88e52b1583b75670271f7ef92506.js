ee6f6b84251c967b9aa90b3b08e7286767f65ef8
Flor Marshall <flordmarshall@gmail.com>
2021-11-09 17:01:50 -0500

    edits for deployment

---
+++ build/precache-manifest.6c1b88e52b1583b75670271f7ef92506.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4417f1d5ef8fea4bbb4e6c28a78719e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea40157fa8bc11cf7d2f",
+    "url": "/static/css/2.93c126cc.chunk.css"
+  },
+  {
+    "revision": "39230f6a7908ed3e065c",
+    "url": "/static/css/main.41f22fd9.chunk.css"
+  },
+  {
+    "revision": "ea40157fa8bc11cf7d2f",
+    "url": "/static/js/2.a2bda0d7.chunk.js"
+  },
+  {
+    "revision": "d57dae176fb87a393a80e02d0e438720",
+    "url": "/static/js/2.a2bda0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39230f6a7908ed3e065c",
+    "url": "/static/js/main.71e87a2c.chunk.js"
+  },
+  {
+    "revision": "0112d8667bc9ec91509a",
+    "url": "/static/js/runtime-main.5c26156e.js"
+  },
+  {
+    "revision": "ee6b6a96f4785f577fc86f99c15e4641",
+    "url": "/static/media/HI-RES-CARTOON_transparent_bg.ee6b6a96.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
